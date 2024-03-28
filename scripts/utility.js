8a341e01142187f34152c970115abd2b8ed38a01
IstiyakIfti <istiyakifti04@gmail.com>
2024-03-28 15:49:50 +0600

    end this project

--- scripts/utility.js
+++ scripts/utility.js
@@ -8,6 +8,33 @@ function showElementByID(elementID){
     element.classList.remove('hidden');
 }
 
+function setBackgroundColorById(elementID){
+    const element = document.getElementById(elementID);
+    element.classList.add("bg-orange-500");
+}
+function removeBackgroundColorById(elementID){
+    const element = document.getElementById(elementID);
+    element.classList.remove("bg-orange-500");
+}
+
+function getTextElementValueById(elementID){
+    const element = document.getElementById(elementID);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementID, value){
+    const element = document.getElementById(elementID);
+    element.innerText = value;
+}
+
+function getElementTextById(elementID){
+    const element = document.getElementById(elementID);
+    const text = element.innerText;
+    return text;
+}
+
 function getARandomAlphabet(){
     // get or create a alphabet array
     const alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -15,7 +42,7 @@ function getARandomAlphabet(){
     // console.log(alphabets);
 
     // get a random between 0-25
-    const randomNumber = Math.random() * 25;
+    const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
     
 
